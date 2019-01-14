921120bfa8701757c429dd96add21b4a3a424e77
Kunitsyn Andrey <contact@libnaru.so>
2019-01-14 14:12:38 +0400

    Create Model 'Book'

---
+++ App/Model/Book.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrey-kun
+ * Date: 13.01.19
+ * Time: 18:30
+ */
+
+namespace App\Model;
+
+use PDO;
+
+class Book extends \Core\Model
+{
+    public static function getAll()
+    {
+        $db = static::getDB();
+        $stmt = $db->query('SELECT name, rating FROM books');
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
