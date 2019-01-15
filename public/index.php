1dc93ef563dacc661091ac9b8a495684a7bb2a17
Kunitsyn Andrey <contact@libnaru.so>
2019-01-15 20:48:12 +0400

    Rename Controller Index by Booklist

--- public/index.php
+++ public/index.php
@@ -12,7 +12,7 @@ error_reporting(E_ALL);
 
 $router = new Core\Router();
 
-$router->add('', ['controller' => 'Index', 'action' => 'index']);
+$router->add('', ['controller' => 'Booklist', 'action' => 'booklist']);
 $router->add('{controller}/{action}');
 
 $router->dispatch($_SERVER['QUERY_STRING']);
