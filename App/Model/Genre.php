7d2ec857b96d614393f38c07cd80aef49d32ac36
Kunitsyn Andrey <contact@libnaru.so>
2019-01-15 20:01:59 +0400

    Create Model 'Genre'

---
+++ App/Model/Genre.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrey-kun
+ * Date: 15.01.19
+ * Time: 19:25
+ */
+
+namespace App\Model;
+
+use PDO;
+
+class Genre extends \Core\Model
+{
+    public static function getAll()
+    {
+        $db = static::getDB();
+        $stmt = $db->query('SELECT * FROM genres ORDER BY rating');
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
