0ea17e50147b69c077f3a795f69224154cb39b8e
Kunitsyn Andrey <contact@libnaru.so>
2019-01-17 21:20:59 +0400

    Core Router: Fix parse query string

--- Core/Router.php
+++ Core/Router.php
@@ -118,7 +118,7 @@ class Router
     protected function removeQueryStringVariables($url)
     {
         if ($url != '') {
-            $parts = explode('&', $url, 2);
+            $parts = explode('?', $url, 2);
             if (strpos($parts[0], '=') === false) {
                 $url = $parts[0];
             } else {
