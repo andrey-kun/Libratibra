d7d22e0049a86a361be44350d6ed9c1900e015fe
Kunitsyn Andrey <contact@libnaru.so>
2019-01-17 21:22:38 +0400

    Model Author: Created construct function

--- App/Model/Author.php
+++ App/Model/Author.php
@@ -8,14 +8,35 @@
 
 namespace App\Model;
 
+use App\ReiterationException;
 use PDO;
 
 class Author extends \Core\Model
 {
+    public $name;
+
     public static function getAll()
     {
         $db = static::getDB();
-        $stmt = $db->query('SELECT * FROM authors ORDER BY rating');
+        $stmt = $db->query('SELECT * FROM authors');
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+
+        $db = static::getDB();
+
+        $stmt = $db->prepare("SELECT * FROM authors WHERE (name=:name)");
+        $stmt->execute((array)$this);
+        $authorsWithSameName = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        if (!empty($authorsWithSameName)) {
+            throw new ReiterationException();
+        }
+
+        $stmt = $db->prepare("INSERT INTO authors (name) values (:name)");
+        $stmt->execute((array)$this);
     }
 }
